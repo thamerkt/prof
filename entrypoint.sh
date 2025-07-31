a51daa1ad91c5622257a0d2fe6aeb709d49f2209
thamerkt <kthirithamer1@gmail.com>
2025-07-31 14:44:51 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "📦 Running Django migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "🚀 Starting Django development server..."
+python manage.py runserver 0.0.0.0:8000
